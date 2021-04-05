cd488153c71368d96ca62a65c64f4de708a3f497
Koen H <gewoon.koen@gmail.com>
2021-04-05 15:02:09 +0200

    bg img, home wrapper

---
+++ src/containers/HomeWrapper.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./HomeWrapper.module.scss";
+
+const HomeWrapper = (props) => (
+  <div className={classes.Body}>
+    <div className={classes.HomeWrapper}></div>
+  </div>
+);
+
+export default HomeWrapper;

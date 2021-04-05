9f44b8a29f0e599eeb880dca759dcdb4e8036473
Koen H <gewoon.koen@gmail.com>
2021-04-05 12:27:31 +0200

    basic folders, assets, bg img, home wrapper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import HomeWrapper from "./containers/HomeWrapper";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomeWrapper />
+    </div>
+  );
 }
 
 export default App;

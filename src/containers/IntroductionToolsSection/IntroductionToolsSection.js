a1a41a7ef4d75aa7803f2245e56479836566a061
Koen H <gewoon.koen@gmail.com>
2021-04-08 12:05:09 +0200

    Intro section - bubble, avatar, toolselector

---
+++ src/containers/IntroductionToolsSection/IntroductionToolsSection.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./IntroductionToolsSection.module.scss";
+import PortraitRight from "../../assets/home/KoenWizardCircle-right.png";
+
+import IntroTextBubble from "../../components/IntroTextBubble/IntroTextBubble";
+import ToolSelector from "../../components/ToolSelector/ToolSelector";
+
+const IntroductionToolsSection = (props) => (
+  <React.Fragment>
+    <IntroTextBubble />
+    <div className={classes.BottomContainer}>
+      <img src={PortraitRight} alt="Wizard with robe and staff" />
+      <ToolSelector />
+    </div>
+  </React.Fragment>
+);
+
+export default IntroductionToolsSection;

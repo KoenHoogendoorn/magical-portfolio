6106b11b33445cfc11c0f105b97210d6262ecee2
Koen H <gewoon.koen@gmail.com>
2021-04-29 14:15:58 +0200

    added selector svg in featured projects selector

--- src/components/FeaturedProjects/FeaturedProject/FeaturedProject.js
+++ src/components/FeaturedProjects/FeaturedProject/FeaturedProject.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import classes from "./FeaturedProject.module.scss";
 
 import Tag from "../../Tag/Tag";
 
 const FeaturedProject = (props) => {
   const FeaturedProjects = [...props.featuredProjects];
+  const [featuredProjectElWidth, setFeaturedProjectElWidth] = useState(0);
+  const FeaturedProjectEl = useRef();
+  useEffect(() => {
+    if (FeaturedProjectEl.current !== undefined) {
+      setFeaturedProjectElWidth(FeaturedProjectEl.current.clientWidth);
+    }
+  }, [FeaturedProjectEl]);
+
   let FeaturedProjectClasses = `${classes.FeaturedProject} `;
 
   if (props.activeProjectId === FeaturedProjects[0].id) {
@@ -55,15 +63,25 @@ const FeaturedProject = (props) => {
     }
   }
 
+  let svgClass = `${classes.ActiveSvg} `;
+
   if (props.activeProjectId !== props.id) {
     FeaturedProjectClasses += `${classes.Inactive} `;
+    svgClass += `${classes.InactiveSvg} `;
   }
+
+  // if (props.activeProjectId === props.id) {
+  //   svgClass += `${classes.ActiveSvg} `
+  // } else {
+
+  // }
 
   return (
     <div
-      className={FeaturedProjectClasses}
       id={props.id}
+      className={FeaturedProjectClasses}
       onClick={props.clicked}
+      ref={FeaturedProjectEl}
     >
       <div className={classes.ProjectInformationContainer}>
         <h3>{props.name}</h3>
@@ -71,6 +89,31 @@ const FeaturedProject = (props) => {
         <Tag bgLight={true}>Design</Tag>
       </div>
       <img src={props.image} alt={props.imageAlt}></img>
+      <svg viewBox="0 0 100 175" width={`${featuredProjectElWidth + 26}px`}>
+        <path
+          d="M83,2 
+              h3
+              a8,8 0 0 1 9,9 
+              v3
+            m0,141.8 
+              v3 
+              a8,8 0 0 1 -9,9 
+              h-3 
+            m-69.8,0 
+              h-3 
+              a8,8 0 0 1 -9,-9 
+              v-3 
+            m0,-141.8 
+              v-3 
+              a8,8 0 0 1 9,-9 
+              h3"
+          fill="none"
+          stroke="#FFFFFF"
+          strokeWidth="4"
+          vectorEffect="non-scaling-stroke"
+          className={svgClass}
+        />
+      </svg>
     </div>
   );
 };

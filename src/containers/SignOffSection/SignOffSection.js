f66a66fc56e331b8d390eb2719fbeaea3945f6e6
Koen H <gewoon.koen@gmail.com>
2021-04-30 13:19:50 +0200

    content update signoff section

--- src/containers/SignOffSection/SignOffSection.js
+++ src/containers/SignOffSection/SignOffSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./SignOffSection.module.scss";
 
 import AvatarRight from "../../assets/home/KoenWizardCircle-right-big.png";
@@ -14,6 +15,20 @@ const SignOffSection = (props) => {
           You can contact me at:{" "}
           <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#104;&#111;&#111;&#103;&#101;&#110;&#100;&#111;&#111;&#114;&#110;&#107;&#111;&#101;&#110;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">
             &#104;&#111;&#111;&#103;&#101;&#110;&#100;&#111;&#111;&#114;&#110;&#107;&#111;&#101;&#110;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;
+          </a>
+        </p>
+        <p>
+          Read more about me here: <Link to={"/about"}>About</Link>
+          {" / "}
+          <a
+            href="https://linkedin.com/in/koen-hoogendoorn-0853a290"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+          {" / "}
+          <a href="https://github.com/KoenHoogendoorn" target="_blank">
+            GitHub
           </a>
         </p>
       </FullWidthTextBubble>

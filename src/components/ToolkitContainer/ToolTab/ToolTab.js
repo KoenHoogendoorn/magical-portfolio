e5dcd720da0749d0223007c3b14bae9f93221334
Koen H <hoogendoornkoen@gmail.com>
2021-05-12 11:18:47 +0200

    finetuning responsiveness

--- src/components/ToolkitContainer/ToolTab/ToolTab.js
+++ src/components/ToolkitContainer/ToolTab/ToolTab.js
@@ -11,7 +11,7 @@ const ToolTab = (props) => {
     <button className={tabClasses} onClick={props.clicked}>
       {props.active ? null : <div className={classes.DarkLayer}></div>}
       <img className={classes.Icon} src={props.icon} />
-      <h3 className={classes.ToolName}>{props.children}</h3>
+      <h3 className={classes.ToolkitName}>{props.children}</h3>
     </button>
   );
 };

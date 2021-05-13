e768837cc6b90aac9ed3d735a2bc1c3733726516
Koen H <hoogendoornkoen@gmail.com>
2021-05-13 16:56:04 +0200

    Game menu

--- src/containers/AdditionalProjectsSection/AdditionalProjectsSection.js
+++ src/containers/AdditionalProjectsSection/AdditionalProjectsSection.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import classes from "./AdditionalProjectsSection.module.scss";
 
+import DragonCircle from "../../assets/home/dragon-game/DragonCircle.svg";
+
+import TextBubbleAvatar from "../../components/TextBubbleAvatar/TextBubbleAvatar";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import Button from "../../components/Button/Button";
 
 const AdditionalProjectsSection = (props) => {
+  const [gameState, setGameState] = useState("unplayed"); // options: unplayed/skipped/running/closed/won/lost
   const projects = props.additionalProjects.map((project) => (
     <div key={`div-${project.id}`} className={classes.AdditionalProject}>
       <ProjectCard
@@ -23,10 +27,43 @@ const AdditionalProjectsSection = (props) => {
     </div>
   ));
 
+  let AdditionalProjectClasses = `${classes.AdditionalProjects} `;
+
+  if (gameState === ("unplayed" || "running")) {
+    AdditionalProjectClasses += `${classes.Darkened} `;
+  }
+
   return (
-    <div className={classes.AdditionalProjectsSection}>
-      <div className={classes.AdditionalProjects}>{projects}</div>
-    </div>
+    <React.Fragment>
+      <TextBubbleAvatar avatarLeft={false}>
+        <h2>
+          Darn, a dragon is blockin the other projects I want to show you.
+        </h2>
+        <p>Help me fight the dragon to free them. </p>
+      </TextBubbleAvatar>
+      <div className={classes.AdditionalProjectsSection}>
+        <div className={classes.GameSection}>
+          <div className={classes.GameMenu}>
+            <img className={classes.DragonCircle} src={DragonCircle}></img>
+            <div className={classes.MenuButtonsContainer}>
+              <Button
+                priority={"primary"}
+                onClick={() => setGameState("running")}
+              >
+                Fight the dragon
+              </Button>
+              <Button
+                priority={"secondary"}
+                onClick={() => setGameState("skipped")}
+              >
+                Skip the fight
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={AdditionalProjectClasses}>{projects}</div>
+      </div>
+    </React.Fragment>
   );
 };
 
